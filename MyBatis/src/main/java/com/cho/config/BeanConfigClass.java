d2ce1770a1d09c555775e277cdd4f606c22e1360
0r0loo <49504411+0r0loo@users.noreply.github.com>
2020-03-31 17:13:37 +0900

    MyBats

---
+++ MyBatis/src/main/java/com/cho/config/BeanConfigClass.java
@@ -0,0 +1,46 @@
+package com.cho.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.cho.mapper.MapperInterface;
+
+@Configuration
+@ComponentScan(basePackages="com.cho.beans")
+public class BeanConfigClass {
+
+	// data source
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName("oracle.jdbc.OracleDriver");
+		source.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		source.setUsername("scott");
+		source.setPassword("tiger");
+		return source;
+	}
+	
+	
+	// SqlSessionFactory : jdbc를 처리하는 객체
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory =factoryBean.getObject();
+		return factory;
+	}
+	
+	// Mapper
+	@Bean
+	public MapperFactoryBean<MapperInterface> test_mapper(SqlSessionFactory factory) throws Exception{
+		MapperFactoryBean<MapperInterface> factoryBean = new MapperFactoryBean<MapperInterface>(MapperInterface.class);
+		factoryBean.setSqlSessionFactory(factory);
+		return factoryBean;
+	}
+	
+}
